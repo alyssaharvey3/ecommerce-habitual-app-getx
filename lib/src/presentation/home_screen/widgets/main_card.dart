e8bda23abc5517507f842e74e9bbcb5addb2af1c
Alyssa Harvey <alyssa.harvey618@gmail.com>
2023-08-02 08:34:13 +0200

    Code refctor

--- lib/src/presentation/home_screen/widgets/main_card.dart
+++ lib/src/presentation/home_screen/widgets/main_card.dart
@@ -1,9 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:habitual/src/presentation/home_screen/widgets/like_button_widget.dart';
 
 import '../../../core/core_export.dart';
+import 'favorite_button_widget.dart';
 
 class MainCard extends StatelessWidget {
   const MainCard({
@@ -24,84 +24,87 @@ class MainCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.topRight,
       children: [
-        SizedBox(
-          width: width ?? 206,
-          height: height ?? 357,
-          child: Card(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(Sizes.p10),
-              ),
+        Card(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(Sizes.p10),
             ),
-            color: cardColor ?? AppColors.blue300,
-            child: InkWell(
-              highlightColor: AppColors.neutral300.withOpacity(.9),
-              borderRadius: const BorderRadius.all(
-                Radius.circular(Sizes.p10),
-              ),
-              onTap: onPressed,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(Sizes.p10),
-                child: Padding(
-                  padding: const EdgeInsets.all(
-                    Sizes.p16,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      gapH24,
-                      Expanded(
-                        child: Center(
-                          child: CachedNetworkImage(
-                            imageUrl: imageUrl,
-                            fit: BoxFit.contain,
-                            placeholder: (_, url) => Center(
-                              child: CircularProgressIndicator.adaptive(
-                                valueColor: AlwaysStoppedAnimation(
-                                  AppColors.neutral800,
-                                ),
-                              ),
-                            ),
+          ),
+          color: cardColor ?? AppColors.blue300,
+          child: InkWell(
+            highlightColor: AppColors.neutral300.withOpacity(.9),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(Sizes.p10),
+            ),
+            onTap: onPressed,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(Sizes.p10),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(
+                  Sizes.p12,
+                  Sizes.p28,
+                  Sizes.p12,
+                  Sizes.p12,
+                ),
+                child: Column(
+                  children: [
+                    gapH24,
+                    CachedNetworkImage(
+                      width: Sizes.deviceWidth * .5,
+                      height: Sizes.deviceHeight * .4 / 2,
+                      imageUrl: imageUrl,
+                      fit: BoxFit.contain,
+                      placeholder: (_, url) => Center(
+                        child: CircularProgressIndicator.adaptive(
+                          valueColor: AlwaysStoppedAnimation(
+                            AppColors.neutral800,
                           ),
                         ),
                       ),
-                      gapH12,
-                      Text(
-                        'Chuck 70 Hi Sneakers Chuck 70 Hi Sneakers',
-                        style: Get.textTheme.displayLarge,
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        // textAlign: TextAlign.center,
-                      ),
-                      gapH4,
-                      Text(
-                        'Converse',
-                        style: Get.textTheme.bodySmall?.copyWith(
-                          fontWeight: Fonts.interRegular,
+                    ),
+                    gapH12,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: Sizes.deviceWidth * .4,
+                          child: Text(
+                            'Chuck 70 Hi Sneakers Chuck 70 Hi Sneakers',
+                            style: Get.textTheme.displayLarge,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            // textAlign: TextAlign.center,
+                          ),
                         ),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      gapH16,
-                      Text(
-                        r'$70.99',
-                        style: Get.textTheme.bodyLarge?.copyWith(
-                          fontWeight: Fonts.interRegular,
+                        gapH4,
+                        Text(
+                          'Converse',
+                          style: Get.textTheme.bodySmall?.copyWith(
+                            fontWeight: Fonts.interRegular,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
                         ),
-                      ),
-                    ],
-                  ),
+                        gapH16,
+                        Text(
+                          r'$70.99',
+                          style: Get.textTheme.bodyLarge?.copyWith(
+                            fontWeight: Fonts.interRegular,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
         ),
-        Positioned(
-          top: Sizes.p4,
-          right: Sizes.p16,
-          child: LikeButtonWidget(
+        Padding(
+          padding: const EdgeInsets.only(right: Sizes.p8),
+          child: FavoriteButton(
             onPressed: () {},
           ),
         )
