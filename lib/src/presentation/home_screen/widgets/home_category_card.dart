4a888749c245e8b7b7200432e01cad8976a451b0
Alyssa Harvey <alyssa.harvey618@gmail.com>
2023-08-02 08:34:30 +0200

    Code refactor

--- lib/src/presentation/home_screen/widgets/home_category_card.dart
+++ lib/src/presentation/home_screen/widgets/home_category_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:habitual/src/common_widgets/common_widgets_export.dart';
 
+import '../../../common_widgets/common_widgets_export.dart';
 import '../../../core/core_export.dart';
 
 class HomeCategoryCard extends StatelessWidget {
@@ -11,63 +11,57 @@ class HomeCategoryCard extends StatelessWidget {
     this.height,
     this.color,
     required this.title,
-    required this.buttonPressed,
-    required this.cardPressed,
+    required this.onTap,
   }) : super(key: key);
 
   final double? width;
   final double? height;
   final Color? color;
   final String title;
-  final VoidCallback? buttonPressed;
-  final VoidCallback? cardPressed;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        SizedBox(
-          width: width ?? 163,
-          height: height ?? 163,
-          child: Card(
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(
-                  Sizes.p10,
-                ),
-              ),
-            ),
-            color: color ?? AppColors.purple300,
-            child: InkWell(
-              borderRadius: const BorderRadius.all(
-                Radius.circular(Sizes.p10),
-              ),
-              onTap: cardPressed,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(Sizes.p10),
-                child: Padding(
-                  padding: const EdgeInsets.all(Sizes.p16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        style: Get.textTheme.displayLarge,
-                        // textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
+        Card(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(
+                Sizes.p10,
               ),
             ),
           ),
-        ),
-        Positioned(
-          right: Sizes.p24,
-          bottom: Sizes.p16,
-          child: PrimaryOutlinedButton(
-            hasText: false,
-            onPressed: buttonPressed,
+          color: color ?? AppColors.purple300,
+          child: InkWell(
+            borderRadius: const BorderRadius.all(
+              Radius.circular(Sizes.p10),
+            ),
+            onTap: onTap,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(Sizes.p10),
+              child: Padding(
+                padding: const EdgeInsets.all(Sizes.p16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: Get.textTheme.displayLarge,
+                    ),
+                    gapH28,
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: PrimaryOutlinedButton(
+                        hasText: false,
+                        onPressed: onTap,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ],
